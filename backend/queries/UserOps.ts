64601be1437ed22d2fd4f134ce56367d29cc79b2
KanTheCodingNoob <s4019548@rmit.edu.vn>
2025-04-16 05:29:02 +0700

    Add get method to user and event

--- backend/queries/UserOps.ts
+++ backend/queries/UserOps.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import g_coDb from "../server/db.ts"
-const g_coEvents = g_coDb.collection("events")
+const g_coUsers = g_coDb.collection("users")
 import g_codes from "../server/statuses.ts"
 
 const g_coRouter = Router()
@@ -8,8 +8,15 @@ const g_coRouter = Router()
 g_coRouter.post("/", function(a_oRequest, a_oResponse) {
 	
 })
-g_coRouter.get("/", function(a_oRequest, a_oResponse) {
-	
+g_coRouter.get("/", async function(a_oRequest, a_oResponse) {
+	try {
+        // Fetch all documents in the "user" collection
+        const l_aUsers = await g_coUsers.find({}).toArray()
+        a_oResponse.status(g_codes("Success")).json(l_aUsers)
+    } catch (err) {
+        console.error("Failed to fetch users:", err)
+        a_oResponse.sendStatus(g_codes("Server error"))
+    }
 })
 g_coRouter.put("/", function(a_oRequest, a_oResponse) {
 	
