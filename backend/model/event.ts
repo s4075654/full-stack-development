c9d8ebf9e93695addbd4a230d71e2e16d6f5b725
KanTheCodingNoob <s4019548@rmit.edu.vn>
2025-04-18 11:01:13 +0700

    Solve the merge conflict

--- backend/model/event.ts
+++ backend/model/event.ts
@@ -3,74 +3,75 @@ export default {
 		$jsonSchema: {
 			bsonType: "object",
 			description: "An event.",
-			required: ["public", "organiser"],
+			required: ["public", "Organiser ID"],
 			properties: {
-				_id: {
-					bsonType: "objectId",
-					description: "The event ID",
-				},
+				_id: {},
 				public: {
 					bsonType: "bool",
-					description: "Whether or not the event is public."
+					title: "Whether or not the event is public."
 				},
 				invitations: {
 					bsonType: "array",
-					description: "The invitations to the event.",
+					title: "The invitations to the event.",
 					uniqueItems: true,
 					items: {
 						bsonType: "objectId",
-						description: "An invitation to the event."
+						title: "An invitation to the event."
 					}
 				},
 				requests: {
 					bsonType: "array",
-					description: "The requests to join the event.",
+					title: "The requests to join the event.",
 					uniqueItems: true,
 					items: {
 						bsonType: "objectId",
-						description: "A request to join the event."
+						title: "A request to join the event."
 					}
 				},
 				"Discussion board": {
 					bsonType: "array",
-					description: "The discussion board of the event.",
+					title: "The discussion board of the event.",
 					uniqueItems: true,
 					items: {
 						bsonType: "objectId",
-						description: "A message in the discussion board."
+						title: "A message in the discussion board."
 					}
 				},
 				notifications: {
 					bsonType: "array",
-					description: "Notifications for the event.",
+					title: "Notifications for the event.",
 					uniqueItems: true,
 					items: {
 						bsonType: "objectId",
-						description: "A notification for the event."
+						title: "A notification for the event."
 					}
 				},
 				images: {
-					bsonType: "objectId",
-					description: "Id of the images of the event.",
+					bsonType: "array",
+					title: "Images of the event.",
+					items: {
+						bsonType: "binData",
+						title: "An image of the event."
+					}
 				},
-				organiser: {
+				"Organiser ID": {
 					bsonType: "objectId",
-					description: "The organiser of the event."
+					title: "The organiser of the event."
 				},
 				eventName: {
 					bsonType: "string",
-					description: "The event name of the event.",
+					title: "The event name of the event."
 				},
 				eventLocation: {
 					bsonType: "string",
-					description: "The event location of the event.",
+					title: "The event location of the event."
 				},
 				eventTime: {
 					bsonType: "string",
-					description: "The time of the event.",
+					title: "The time of the event."
 				}
 			},
-			additionalProperties: false,
+			additionalProperties: false
 		}
 	}
 }
