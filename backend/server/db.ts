d7145f86334b0960da7729e76d945bbec1a9844e
KanTheCodingNoob <s4019548@rmit.edu.vn>
2025-04-18 10:58:58 +0700

    Integrate GridFS into the database setup

--- backend/server/db.ts
+++ backend/server/db.ts
@@ -1,25 +1,30 @@
 import "dotenv/config"
-
 import { MongoClient } from "mongodb"
-globalThis.g_oConnection = new MongoClient("mongodb://" + process.env.DB_HOST + ":" + process.env.DB_PORT)
 
-const g_coDb = await globalThis.g_oConnection.db(process.env.DB_NAME)
+globalThis.g_oConnection = new MongoClient("mongodb://" + process.env.m_sDbHost + ":" + process.env.m_sDbPort)
+const g_coDb = await globalThis.g_oConnection.db(process.env.m_sDbName)
 
 import { readdir } from "fs/promises"
-import {setupGridFSBucket} from "./gridfs.ts";
+import { setupGridFSBucket } from "./gridfs";
 
-setupGridFSBucket(g_coDb)
+let g_vReadLine, g_vInterface
 for (const l_csFileName of await readdir("backend/model")) {
-	const l_coMod = await import("../model/" + l_csFileName)
+	const l_coMod = (await import("../model/" + l_csFileName)).default
 	const l_csCollectionName = l_csFileName.replace(".ts", "s")
-	if ((await g_coDb.listCollections({ name: l_csCollectionName }).toArray()).length === 0) {
-		//	Validation Setup: Recreates collections with schema validators
-		await g_coDb.createCollection(l_csCollectionName, l_coMod.default)
-		console.log("Created " + l_csCollectionName + ".")
-	} else {
-		console.log("The \"" + l_csCollectionName + "\" collection already exists.")
+	switch ((await g_coDb.listCollections({ name: l_csCollectionName }).toArray()).length === 0) {
+		case false:
+			if (!("m_sDev" in process.env)) {
+				console.warn("Existing " + l_csCollectionName + " ignored.")
+				break
+			}
+			await g_coDb.dropCollection(l_csCollectionName)
+			console.log("Dropped " + l_csCollectionName + ".")
+		case true:
+			await g_coDb.createCollection(l_csCollectionName, l_coMod.default)
+			console.log("Created " + l_csCollectionName + ".")
 	}
 }
+setupGridFSBucket(g_coDb)
 console.log("Registered collections.")
 
 export default g_coDb
